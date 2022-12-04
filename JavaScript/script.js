6e61644e32e71b30516059c4d7f3abc681a8bd52
Arsik3204 <arsik3204@MBP-Arsik3204.Dlink>
2022-12-04 19:41:58 +0300

    Early

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function btn() {
+  let username = document.querySelector(".username").value;
+  console.log(username);
+
+  let password = document.querySelector(".password").value;
+  console.log(password);
+}
+
